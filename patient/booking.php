5a61a9e612b6b1f5c91b85e330649363cd947341
POLOK PODDAR <89349568+proloypoddar@users.noreply.github.com>
2024-09-23 23:52:22 +0600

    main

--- patient/booking.php
+++ patient/booking.php
@@ -250,7 +250,7 @@
                                                             Session Title: '.$title.'<br>
                                                             Session Scheduled Date: '.$scheduledate.'<br>
                                                             Session Starts : '.$scheduletime.'<br>
-                                                            Channeling fee : <b>LKR.2 000.00</b>
+                                                            Channeling fee : <b>BDT : 500</b>
 
                                                         </div>
                                                         <br>
